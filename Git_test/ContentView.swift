00aa2a297ed7f4cfaaa04c40fad9d13497d7968b
ohkong <ohkong@gmail.com>
2019-12-18 10:04:20 +0900

    Initial Commit

---
+++ Git_test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Git_test
+//
+//  Created by cmt-mac-01 on 18/12/2019.
+//  Copyright © 2019 cmt-mac-01. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
